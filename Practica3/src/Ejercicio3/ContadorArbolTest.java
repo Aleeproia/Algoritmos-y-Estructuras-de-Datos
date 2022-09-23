89aeb76c86184f11d680dc5fda450a430cad0f26
Alejandro Proia <62100665+Aleeproia@users.noreply.github.com>
2022-09-23 14:03:21 -0300

    Update ContadorArbolTest.java

--- Practica3/src/Ejercicio3/ContadorArbolTest.java
+++ Practica3/src/Ejercicio3/ContadorArbolTest.java
@@ -6,38 +6,39 @@ import Ejercicio2.ListaEnlazadaGenerica;
 public class ContadorArbolTest {
 
 	public static void main(String[] args) {
+		
 		ArbolBinario<Integer> a = new ArbolBinario<Integer>(2);
-        ArbolBinario<Integer> b = new ArbolBinario<Integer>(4);
-        ArbolBinario<Integer> c = new ArbolBinario<Integer>(3);
-        ArbolBinario<Integer> d = new ArbolBinario<Integer>(6);
-        ArbolBinario<Integer> e = new ArbolBinario<Integer>(10);
-        ArbolBinario<Integer> f = new ArbolBinario<Integer>(5);
-        ArbolBinario<Integer> g = new ArbolBinario<Integer>(7);
-        
-        a.agregarHijoIzquierdo(b);
-        a.agregarHijoDerecho(c);
-        b.agregarHijoIzquierdo(d);
-        b.agregarHijoDerecho(g);
-        c.agregarHijoIzquierdo(e);
-        c.agregarHijoDerecho(f);
-        
-        ContadorArbol cont1 = new ContadorArbol();
-        ContadorArbol cont2 = new ContadorArbol();
-        ContadorArbol cont3 = new ContadorArbol();
-        
-        ListaEnlazadaGenerica<Integer> lista1 = cont1.numerosParesPreOrden(a);
-        ListaEnlazadaGenerica<Integer> lista2 = cont2.numerosParesInOrden(a);
-        ListaEnlazadaGenerica<Integer> lista3 = cont3.numerosParesPostOrden(a);
-        
-        lista1.comenzar();
-        lista2.comenzar();
-        lista3.comenzar();
-        
-        for (int i = 1; i <= lista1.tamanio(); ++i) {
-            System.out.println(lista1.elemento(i));
-            System.out.println(lista2.elemento(i));
-            System.out.println(lista3.elemento(i));
-        }
+		ArbolBinario<Integer> b = new ArbolBinario<Integer>(4);
+		ArbolBinario<Integer> c = new ArbolBinario<Integer>(3);
+		ArbolBinario<Integer> d = new ArbolBinario<Integer>(6);
+		ArbolBinario<Integer> e = new ArbolBinario<Integer>(10);
+		ArbolBinario<Integer> f = new ArbolBinario<Integer>(5);
+		ArbolBinario<Integer> g = new ArbolBinario<Integer>(7);
+
+		a.agregarHijoIzquierdo(b);
+		a.agregarHijoDerecho(c);
+		b.agregarHijoIzquierdo(d);
+		b.agregarHijoDerecho(g);
+		c.agregarHijoIzquierdo(e);
+		c.agregarHijoDerecho(f);
+
+		ContadorArbol cont1 = new ContadorArbol();
+		ContadorArbol cont2 = new ContadorArbol();
+		ContadorArbol cont3 = new ContadorArbol();
+
+		ListaEnlazadaGenerica<Integer> lista1 = cont1.numerosParesPreOrden(a);
+		ListaEnlazadaGenerica<Integer> lista2 = cont2.numerosParesInOrden(a);
+		ListaEnlazadaGenerica<Integer> lista3 = cont3.numerosParesPostOrden(a);
+
+		lista1.comenzar();
+		lista2.comenzar();
+		lista3.comenzar();
+
+		for (int i = 1; i <= lista1.tamanio(); ++i) {
+		    System.out.println(lista1.elemento(i));
+		    System.out.println(lista2.elemento(i));
+		    System.out.println(lista3.elemento(i));
+		}
 
 
 	}
